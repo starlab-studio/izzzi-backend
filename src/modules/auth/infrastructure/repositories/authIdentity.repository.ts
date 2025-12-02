5063a764f93a4cea033e1a8970a2c34ff16ad644
Omer Dotchamou <omer.dotchamou@scalens.com>
2025-12-02 23:52:52 +0100

    Implement sign up confirmation logic in custom auth adapter

--- src/modules/auth/infrastructure/repositories/authIdentity.repository.ts
+++ src/modules/auth/infrastructure/repositories/authIdentity.repository.ts
@@ -1,52 +1,64 @@
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
 
-import { IAuthIdentity } from "../../domain/types";
-import { AuthIdentity } from "../models/authIdentity.model";
+import { AuthIdentityName, IAuthIdentity } from "../../domain/types";
+import { AuthIdentityEntity } from "../../domain/entities/authIdentity.entity";
+import { AuthIdentityModel } from "../models/authIdentity.model";
 import { IAuthIdentityRepository } from "../../domain/repositories/authIdentity.repository";
 
 export class AuthIdentityRepository implements IAuthIdentityRepository {
   constructor(
-    @InjectRepository(AuthIdentity)
+    @InjectRepository(AuthIdentityModel)
     private ormRepository: Repository<IAuthIdentity>
   ) {}
 
-  async create(data: Partial<IAuthIdentity>): Promise<IAuthIdentity> {
-    const entity = this.ormRepository.create({
-      username: data.username,
-      password: data.password ?? null,
-      provider: data.provider,
-      providerUserId: data.providerUserId,
-    });
+  async create(entity: AuthIdentityEntity): Promise<AuthIdentityEntity> {
+    const data = entity.toPersistence();
+    const ormEntity = this.ormRepository.create(data);
+    const saved = await this.ormRepository.save(ormEntity);
 
-    return await this.ormRepository.save(entity);
+    return AuthIdentityEntity.reconstitute(saved);
   }
 
-  async findById(id: string): Promise<IAuthIdentity | null> {
-    return await this.ormRepository.findOne({ where: { id } });
+  async findById(id: string): Promise<AuthIdentityEntity | null> {
+    const ormEntity = await this.ormRepository.findOne({ where: { id } });
+    if (!ormEntity) return null;
+
+    return AuthIdentityEntity.reconstitute(ormEntity);
   }
 
-  async findByUsername(username: string): Promise<IAuthIdentity | null> {
-    return await this.ormRepository.findOne({ where: { username } });
+  async findByUsername(username: string): Promise<AuthIdentityEntity | null> {
+    const ormEntity = await this.ormRepository.findOne({ where: { username } });
+    if (!ormEntity) return null;
+
+    return AuthIdentityEntity.reconstitute(ormEntity);
   }
 
   async findByProviderAndUsername(
-    provider: string,
+    provider: AuthIdentityName,
     username: string
-  ): Promise<IAuthIdentity | null> {
-    return await this.ormRepository.findOne({ where: { provider, username } });
+  ): Promise<AuthIdentityEntity | null> {
+    const ormEntity = await this.ormRepository.findOne({
+      where: { provider, username },
+    });
+    if (!ormEntity) return null;
+
+    return AuthIdentityEntity.reconstitute(ormEntity);
   }
 
-  async findAll(): Promise<IAuthIdentity[]> {
-    return await this.ormRepository.find();
+  async findAll(): Promise<AuthIdentityEntity[]> {
+    const ormEntityList = await this.ormRepository.find();
+
+    return ormEntityList.map((ormEntity) =>
+      AuthIdentityEntity.reconstitute(ormEntity)
+    );
   }
 
-  async update(
-    id: string,
-    entity: Pick<IAuthIdentity, "provider" | "providerUserId">
-  ): Promise<IAuthIdentity> {
-    await this.ormRepository.update(id, entity);
-    return (await this.findById(id)) as IAuthIdentity;
+  async save(entity: AuthIdentityEntity): Promise<AuthIdentityEntity> {
+    const data = entity.toPersistence();
+    const saved = await this.ormRepository.save(data);
+
+    return AuthIdentityEntity.reconstitute(saved);
   }
 
   async delete(id: string): Promise<void> {

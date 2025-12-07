cda6ed5d6fa955f306bfb1dea7cbb3a40d11a246
Omer Dotchamou <omer.dotchamou@scalens.com>
2025-12-07 01:52:52 +0100

    Implement api route for getting user membership details

--- src/modules/organization/domain/repositories/membership.repository.ts
+++ src/modules/organization/domain/repositories/membership.repository.ts
@@ -3,8 +3,10 @@ import { MembershipEntity } from "../entities/membership.entity";
 
 export interface IMembershipRepository extends IRepository<MembershipEntity> {
   create(data: MembershipEntity): Promise<MembershipEntity>;
-  findByUser(userId: string): Promise<MembershipEntity[] | []>;
   findByOrganization(organizationId: string): Promise<MembershipEntity[] | []>;
+  findByUserIdWithOrganizations(
+    userId: string
+  ): Promise<MembershipEntity[] | []>;
   findByUserAndOrganization(
     userId: string,
     organizationId: string
